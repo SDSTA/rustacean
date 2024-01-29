0df18e76e10320d2b8d3d1077d1144c143972e39
SDSTA <superdevspace@gmail.com>
2024-01-29 15:08:43 -0500

    cargo cleaned

--- hello-cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-F:\rustacean\hello-cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-F:\rustacean\hello-cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:
--- hello-cargo/target/debug/hello-cargo.d
+++
@@ -1,1 +0,0 @@
-F:\rustacean\hello-cargo\target\debug\hello-cargo.exe: F:\rustacean\hello-cargo\src\main.rs
