d3448a470b4dc413db5f84d2bbf7114ee0b78e2c
SDSTA <superdevspace@gmail.com>
2024-01-29 15:03:15 -0500

    cargo first project completed

---
+++ hello-cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+F:\rustacean\hello-cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+F:\rustacean\hello-cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+F:\rustacean\hello-cargo\target\debug\hello-cargo.exe: F:\rustacean\hello-cargo\src\main.rs
